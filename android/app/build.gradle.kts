e0bc62c5c5d38a94ef5ba9c55f9812e3c17c9fc6
bjorn <bjorksander@gmail.com>
2026-07-20 18:47:31 +0700

    Bump ke v2.9.1 (versionCode 12)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bjorn.claudepad"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "2.9"
+        versionCode = 12
+        versionName = "2.9.1"
     }
 
     buildTypes {
